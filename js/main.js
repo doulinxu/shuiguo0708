05149879724893758ddff1b570f1d03999a0f34a
347247864@qq.com <linxu.dou@vcg.com>
2020-07-13 23:33:31 +0800

    ftp获取列表 生产环境接口地址分开

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.documentElement.style.fontSize = window.innerWidth / 3.75 + 'px';
+$('img').lazyload(); //延迟加载
+//if(!/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+//	$('#container').width('500px').css({'margin':'0 auto'});
+//	
+//}	 
